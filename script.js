0d304fcfeefec5e817b10b5b3e43f44b4889f000
cldevrun <clement.leung96@gmail.com>
2020-05-30 23:15:09 -0700

    Blank canvas appearance

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
+//const inputSize = document.querySelector();
+const container = document.querySelector('.container');
+
+makeGrid(50);
+
 function makeGrid(e){
-    let container = document.querySelector('.container');
-    for (i = 0; i < e; i++){
-        let content = document.createElement('div');
-        content.classList.add('row'+i);
-        content.style.display = "inline-block";
-        content.style.height = (800)/e + "px";
-        content.style.width = (800)/e + "px";
-        for( j = 0; j < e; j++){
-            let crow = document.createElement('div'+j);
-            crow.style.display = "inline-block";
-            crow.style.height = (800)/e + "px";
-            crow.style.width = (800)/e + "px";
-            crow.style.backgroundColor = "blue";
-            crow.style.justifyContent = "center";
-            content.appendChild(crow);
-        }
-        //content.style.flex = "1 0 "+100/e + "px";
-        
+    let pixLength = (getComputedStyle(container).minWidth.replace('px','')/e)+'px'; 
+    console.log(pixLength);
+    for (i = 0; i < e*e; i++){
+        const content = document.createElement('div');
+        content.style.width = pixLength;
+        content.style.height = pixLength;
         container.appendChild(content);
     }
 }
 
-makeGrid(5);
+/* Supports sizes 1-200, if none given default to 50 */
+function resetGrid() {
+    size = inputSize.value;
+    clearGrid();
+    makeGrid(size);
+}
+
+/* Function to clearGrid to fresh clean state */
+function clearGrid() {
+    Array.from(document.querySelectorAll('.pixel')).forEach(pixel => container.removeChild(pixel));
+}
