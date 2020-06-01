5cd7cac60365bebeaaa566dd3838796c2604b7f0
cldevrun <clement.leung96@gmail.com>
2020-05-31 21:46:23 -0700

    Add grid resizing and coloring when mouse hovers over grid.

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
+let hslCount = 0;
+
 //const inputSize = document.querySelector();
 const container = document.querySelector('.container');
+const reset  = document.querySelector('.btn_reset');
+const resize = document.querySelector('.btn_resize');
+const input = document.querySelector('.input');
 
-makeGrid(50);
+reset.addEventListener('click', clearGrid);
+resize.addEventListener('click', resetGrid);
+makeGrid(3);
 
 function makeGrid(e){
-    let pixLength = (getComputedStyle(container).minWidth.replace('px','')/e)+'px'; 
-    console.log(pixLength);
+    const pixLength = (getComputedStyle(container).minWidth.replace('px','')/e)+'px'; 
     for (i = 0; i < e*e; i++){
-        const content = document.createElement('div');
+        let content = document.createElement('div');
         content.style.width = pixLength;
         content.style.height = pixLength;
+        content.classList.add('pix');
+        content.addEventListener('mouseover', drawColor);
         container.appendChild(content);
     }
 }
 
+function drawColor(e) {
+    let color = getColor();
+    e.target.style.backgroundColor = color;
+}
+
+function getColor() {
+    let color = 'hsl('+ hslCount +', 100%, 50%)';
+    if (hslCount == 360) {
+        hslCount = 0;
+    } else {
+        hslCount += 10;
+    }
+    return color;
+}
+
 /* Supports sizes 1-200, if none given default to 50 */
 function resetGrid() {
-    size = inputSize.value;
-    clearGrid();
+    size = input.value;
+    Array.from(document.querySelectorAll('.pix')).forEach(pixel => container.removeChild(pixel));
     makeGrid(size);
 }
 
 /* Function to clearGrid to fresh clean state */
 function clearGrid() {
-    Array.from(document.querySelectorAll('.pixel')).forEach(pixel => container.removeChild(pixel));
+    Array.from(document.querySelectorAll('.pix')).forEach(pixel => pixel.style.backgroundColor='transparent');
 }
